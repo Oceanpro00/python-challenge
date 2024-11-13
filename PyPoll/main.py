c9055a06c8a4ca680a9bba4134e8b3ffdbdb2d30
sean.schallberger@gmail.com <sean.schallberger@gmail.com>
2024-11-13 18:42:16 -0500

    Save/Upload - PyPoll(75% DONE)

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,10 +12,11 @@ txt_output = os.path.join("analysis", "PyPoll_Analysis.txt")
 
 total_Votes = 0
 candidates = []
+unique_candidates = []
+votes_count = 0
 votes_counted = []
 percentage_voted = []
-total_Votes_Candidates = []
-result = {}
+txt_lines_2 = []
 
 # Open and read the Input Csv file
 
@@ -41,17 +42,55 @@ with open (csv_input, encoding="UTF-8") as csv_data:
 
 	# List all Unique Candidates
 
-	candidates = list(set(candidates))
+	unique_candidates = list(set(candidates))
 
 	# a For loop to go through each candidate listed
 
-	# for cand in candidates:
+	for x in unique_candidates:
+	
+		# a For loop to go through and calculate the number of votes per candidate
+
+		for i in candidates:
+			if i == x:
+				votes_count += 1
+
+		# votes_count = [votes_count += 1 for i in candidates if i == x]
+
+		# Define the votes per Candidate
+
+		votes_counted.append(votes_count)
+
+		# Calculate and Define the percentage voted per Candidate and reset vote counter for next Candidate
+
+		percentage_voted.append(round(((votes_count/total_Votes)*100),3))
+		votes_count = 0
+	
+
+# Preparing Executable Text Lines for Analysis
+
+txt_lines_1 = ["Election Results", "",
+            "--------------------------------------", "",
+            f"Total Votes: {total_Votes}", "",
+            "--------------------------------------", "",]
+
+for i in range(len(unique_candidates)):
+    txt_lines_2.append(f"{unique_candidates[i]}: {percentage_voted[i]}% ({votes_counted[i]})")
+
+txt_lines_3 = ["--------------------------------------", "",
+            "Winner :"]
+
+# new function to print and add line breaks
+
+def execute_Print(x):
+
+	for line in x:
+		print(line)
+	print("") 
 
 
-	# 	# votes_counted.append([len(i) for i in read if i[2] == cand])
 
-	# 	result[cand] = votes_counted
+# Terminal Print Out - ( print("") are for legibility )
 
-# print(result)
-
-print(f"{total_Votes} total votes among these candidates : {candidates} and the result is {result}")
+execute_Print(txt_lines_1)
+execute_Print(txt_lines_2)
+execute_Print(txt_lines_3)
